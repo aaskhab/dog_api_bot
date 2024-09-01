84d54db74752414d0340314b354fe9d791f2064a
aaskhab <kularinskiy22@gmail.com>
2024-09-01 18:19:22 +0300

    split cat and dog

--- api_bot/routers/user.py
+++ api_bot/routers/user.py
@@ -2,7 +2,7 @@ from aiogram import Bot, Router
 from aiogram.filters import CommandStart, Command
 from aiogram.types import Message
 
-from api_bot.utils.dog_api import get_dog_url
+from api_bot.utils.api import get_dog_url, get_cat_url
 
 user_router = Router()
 
@@ -11,8 +11,14 @@ async def start_command(message: Message):
     await message.answer(text='Добро пожаловать в API бота.\nВ нашем боте по разным команадам '
                                 'можно обращаться к разным API сервисам.\n\n<b>Напишите команду /api для теста</b>')
 
-@user_router.message(Command('api'))
-async def api_command(message: Message, bot: Bot):
+@user_router.message(Command('dog'))
+async def dog_image(message: Message, bot: Bot):
     dog_url = await get_dog_url()
 
     await bot.send_photo(chat_id=message.chat.id, photo=dog_url)
+
+@user_router.message(Command('cat'))
+async def cat_image(message: Message, bot: Bot):
+    cat_url = await get_cat_url()
+
+    await bot.send_photo(chat_id=message.chat.id, photo=cat_url)
--- api_bot/utils/api.py
+++ api_bot/utils/api.py
@@ -14,3 +14,13 @@ async def get_dog_url():
             return response['message']
         else:
             logging.info('Wrong response: %s', response)
+
+async def get_cat_url():
+    url = 'https://api.thecatapi.com/v1/images/search'
+
+    async with ClientSession() as session:
+        response = await session.get(url=url)
+
+        response = await response.json()
+
+        return response[0]['url']
